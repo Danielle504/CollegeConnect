50313f9027b525dd36f21fb66d7849c1bbd03d0d
Blake Wyatt <nabakin@protonmail.ch>
2019-11-12 02:35:03 -0500

    Add database connection

--- API/app.js
+++ API/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+const mysql = require('mysql');
+const db_config = require('./db_config');
+
+let connection = mysql.createConnection({
+    host: db_config.host,
+    user: db_config.user,
+    password: db_config.password,
+    database: db_config.database
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+ 
+  console.log('Connected to the MySQL server.');
+});
+
 // import morgan package
 const morgan = require('morgan');
 //use it
